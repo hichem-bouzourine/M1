5e7f0e13d36d0a119204d6f96a2bef74cf2db128
Hichem BOUZOURINE <hichembouzourine.uni@gmail.com>
2024-02-12 18:35:12 +0100

    TME 3 PC3R - fin.

--- S2/PC3R/TMEs/TME3/main.go
+++ S2/PC3R/TMEs/TME3/main.go
@@ -1,90 +1,138 @@
 /*
-*	le code de lecture à partir du code est fait avec l'aide du ChatGPT
-*
+*	quelques parties du code comme le calcule de la difference des dates + lecture à partir d'un fichier sont fournis par *ChatGPT*
+* 	mais le reste c'est perso.
 */
 
-
-package main;
+package main
 
 import (
-    "fmt"
-    "time"
-    "strings"
-);
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
 
 type paquet struct {
-	fruit string
-	entier int
+	departTime string
+	arriveTime string
+	arret  int
 }
 
-struct requete { 
-	paquet paquet
-}
-
-func lecteur (fileParh string, c chan paquet) {
-	// ouverture du ficher
-    file, err := os.Open(filePath)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
-
-	for fichier >> {
-
-		// lecture ligne par ligne
-		
-		// envoyer chaque ligne a un travailleur
-		c <- // la donnée à envoyer 
+func lecteur(canal_worker chan string) {
+	file, err := os.Open("./stop_times.txt")
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	for scanner.Scan() {
+		//envoi du text par le canal
+		canal_worker <- scanner.Text()
+	}
+
 }
 
-
-func serverCalcule() {
-	
+func calculateDifferenceDates(start string, end string) int {
+	arrive, _ := time.Parse("15:04:05", start)
+	departed, _ := time.Parse("15:04:05", end)
+	diff := departed.Sub(arrive)
+	return int(diff.Minutes())
 }
 
-func redacteur() {
-	fin := make(chan int)
+func getPaquetInformations(c string) (string, string, int) {
+	values := strings.Split(c, ",")
+	arriveTime := values[1]
+	departTime := values[2]
+	diffMinutes := calculateDifferenceDates(arriveTime, departTime)
+	return arriveTime, departTime, diffMinutes
+}
 
+func worker(c_lecteur chan string, canal_calculeServer chan chan interface{}, c_reducteur chan chan interface{}) {
 	for true {
-		//
+		// stocker le texte qu'on a lu par le canal dans un variable 
+		text := <-c_lecteur
+		arriveTime, departTime, _ := getPaquetInformations(text)
+
+		// Envoi du paquet au serveur de calcul
+		c_privee_calculeServer := make(chan interface{})
+		canal_calculeServer <- c_privee_calculeServer
+		c_paquet_calculeServer := make(chan paquet)
+		c_privee_calculeServer <- c_paquet_calculeServer
+
+		pq := paquet{departTime: departTime, arriveTime: arriveTime, arret: 0}
+		c_paquet_calculeServer <- pq
+
+		pq_calculeServer := <-c_paquet_calculeServer
+		fmt.Println("Worker a reçu le paquet depuis le serveur, en cour de l'envoi au réducteur")
+
+		// Envoi du paquet au réducteur
+		c_privee_reducteur := make(chan interface{})
+		c_reducteur <- c_privee_reducteur
+		c_paquet_reducteur := make(chan paquet)
+		c_privee_reducteur <- c_paquet_reducteur
+
+		c_paquet_reducteur <- pq_calculeServer
 	}
-	
 }
 
-func prod(c chan paquet, calc chan paquet , cc chan chan interface {}) {
-	// il faut éviter d'utiliser un seul canal partagé par tout, parce que ça sera mélangé
+
+/*
+*	server qui traite les demandes et retourne la difference
+*/
+func calculeServer(canal_calculeServer chan chan interface{}) {
 	for true {
-		fr := fruit[rand.]
+		canal_worker := <-canal_calculeServer
+		canal_worker_paquet := (<-canal_worker).(chan paquet)
+		mon_paquet := <-canal_worker_paquet
+		fmt.Println("le calcule de :", mon_paquet.departTime, " <--> ", mon_paquet.arriveTime)
+		diffMinutes := calculateDifferenceDates(mon_paquet.arriveTime, mon_paquet.departTime)
+		mon_paquet.arret = diffMinutes
+		canal_worker_paquet <- mon_paquet
 	}
-
-
-	// creation et envoi sur le canal 
-	mon_c := make(chan interface {})
-	mon_c <- 1
-	mon_c <- "toto"
 }
 
-func cons(c chan paquet, cc chan chan interface {}) {
+
+func reducteur(c_reducteur chan chan interface{}, canal_signal_fin chan int) {
+	somme := 0
 	for true {
-		P:= <- c
-		P.fruit 
-		
-		fmt.Println("consommateur")
+		select {
+		// recoit des paquets
+		case canal_privee_worker := <-c_reducteur:
+			{
+				canal_worker_paquet := (<-canal_privee_worker).(chan paquet)
+				pq := (<-canal_worker_paquet)
+				somme = somme + pq.arret
+			}
+		case <-canal_signal_fin: // recevoir le signal de fin du travail --> return la somme
+			{
+				canal_signal_fin <- somme
+				return
+			}
+		}
 	}
-
-
-	// 
-	temp:= <- cc
-	
-
 }
 
 func main() {
-	NB_TRAVAILLEURS := 5
+	workers := 4
+	canal_worker_lecteur := make(chan string)
+	canal_calculeServer := make(chan chan interface{})
+	c_reducteur := make(chan chan interface{})
+	canal_signal_fin := make(chan int)
 
-	// cree les canaux 
+	go func() { calculeServer(canal_calculeServer) }()
+	go func() { reducteur(c_reducteur, canal_signal_fin) }()
+	go func() { lecteur(canal_worker_lecteur) }()
+	// chaque worker execute le code de la fonction worker
+	for i := 0; i < workers; i++ {
+		go func() { worker(canal_worker_lecteur, canal_calculeServer, c_reducteur) }()
+	}
+	time.Sleep(10 * time.Second)
 
-
+	canal_signal_fin <- 0
+	total := (<-canal_signal_fin)
+	fmt.Println("ça nous a pris -> ", total)
 
 }
